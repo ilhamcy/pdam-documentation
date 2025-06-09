1c2d58d2674d1e79663a5307b11019639f4e0aea
ilham cantayuda <ilhamcantayuda@gmail.com>
2025-06-09 22:00:06 +0700

    home

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'PDAM Documentations',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'PT. Devetek Marapi Inovasi',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -9,32 +9,78 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+
+        <div className={styles.row_custom}>
+
+          <div className={styles.buttons_custom}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Baca Meter
+            </Link>
+          </div>
+
+          <div className={styles.buttons_custom}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Penagihan
+            </Link>
+          </div>
+
+          <div className={styles.buttons_custom}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Kepegawaian
+            </Link>
+          </div>
+
+          <div className={styles.buttons_custom}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Non Air
+            </Link>
+          </div>
+
+          <div className={styles.buttons_custom}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Pergudangan
+            </Link>
+          </div>
+
+          <div className={styles.buttons_custom}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Sensor
+            </Link>
+          </div>
+
         </div>
+
       </div>
     </header>
   );
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="PDAM Documentations">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
